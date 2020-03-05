9c8d026b67c1e4d8fe9a1dda1fbadad403d521d1
邹汉章 <45626756+Optimus-Zhang-Prime@users.noreply.github.com>
2020-03-05 21:47:44 +0800

    启动定时

--- src/main/java/com/wizz/Application.java
+++ src/main/java/com/wizz/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableScheduling
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
