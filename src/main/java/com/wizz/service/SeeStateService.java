53155eb0362ab674a84e65686030a4b79193a4eb
邹汉章 <45626756+Optimus-Zhang-Prime@users.noreply.github.com>
2020-03-06 20:46:15 +0800

    父级组织名（有疑问）

--- src/main/java/com/wizz/service/SeeStateService.java
+++ src/main/java/com/wizz/service/SeeStateService.java
@@ -33,71 +33,71 @@ public class SeeStateService {//按组织查看疫情信息
     @Autowired
     CloudFunctionUtils cloudFunctionUtils;
 
-    public List<User> getIllUser(Integer orggrade, String orgid) {
+    public List<User> getIllUser(Integer orggrade, String orgfathername,String orgname) {
         if (orggrade == 1) {
-            return userStateDao.getClassAilluser(orgid);//获取一级组织生病的学生
+            return userStateDao.getClassAilluser(orgfathername,orgname);//获取一级组织生病的学生
         } else if (orggrade == 2) {
-            return userStateDao.getClassBilluser(orgid);
+            return userStateDao.getClassBilluser(orgfathername,orgname);
         } else {
-            return userStateDao.getClassCilluser(orgid);
+            return userStateDao.getClassCilluser(orgfathername,orgname);
         }
 
     }
 
-    public List<User> getHdangerUser(Integer orggrade, String orgid) {
+    public List<User> getHdangerUser(Integer orggrade,String orgfathername, String orgid) {
         if (orggrade == 1) {
-            return userStateDao.getClassAHdangeruser(orgid);//获取高度易感用户
+            return userStateDao.getClassAHdangeruser(orgfathername,orgid);//获取高度易感用户
         } else if (orggrade == 2) {
-            return userStateDao.getClassBHdangeruser(orgid);//
+            return userStateDao.getClassBHdangeruser(orgfathername,orgid);//
         } else {
-            return userStateDao.getClassCHdangeruser(orgid);
+            return userStateDao.getClassCHdangeruser(orgfathername,orgid);
         }
 
     }
 
-    public List<User> getMdangerUser(Integer orggrade, String orgid) {
+    public List<User> getMdangerUser(Integer orggrade,String orgfathername, String orgid) {
         if (orggrade == 1) {
-            return userStateDao.getClassAMdangeruser(orgid);//获取易感用户
+            return userStateDao.getClassAMdangeruser(orgfathername,orgid);//获取易感用户
         }
         if (orggrade == 2) {
-            return userStateDao.getClassBMdangeruser(orgid);
+            return userStateDao.getClassBMdangeruser(orgfathername,orgid);
         } else {
-            return userStateDao.getClassCMdangeruser(orgid);
+            return userStateDao.getClassCMdangeruser(orgfathername,orgid);
         }
 
     }
 
-    public List<User> getLdangerUser(Integer orggrade, String orgid) {
+    public List<User> getLdangerUser(Integer orggrade,String orgfathername, String orgid) {
         if (orggrade == 1) {
-            return userStateDao.getClassALdangeruser(orgid);//获取无风险用户
+            return userStateDao.getClassALdangeruser(orgfathername,orgid);//获取无风险用户
         }
         if (orggrade == 2) {
-            return userStateDao.getClassBLdangeruser(orgid);
+            return userStateDao.getClassBLdangeruser(orgfathername,orgid);
         } else {
-            return userStateDao.getClassCLdangeruser(orgid);
+            return userStateDao.getClassCLdangeruser(orgfathername,orgid);
         }
     }
-    public List<User> getSuspectedUser(Integer orggrade, String orgid) {
+    public List<User> getSuspectedUser(Integer orggrade, String orgfathername,String orgid) {
         if (orggrade == 1) {
-            return userStateDao.getClassASuspectedUser(orgid);//获取疑似用户，90-100
+            return userStateDao.getClassASuspectedUser(orgfathername,orgid);//获取疑似用户，90-100
         }
         if (orggrade == 2) {
-            return userStateDao.getClassBSuspectedUser(orgid);
+            return userStateDao.getClassBSuspectedUser(orgfathername,orgid);
         } else {
-            return userStateDao.getClassCSuspectedUser(orgid);
+            return userStateDao.getClassCSuspectedUser(orgfathername,orgid);
         }
     }
 
 
 
-    public List<User> getAllUser(Integer orggrade, String orgid,Integer page) {
+    public List<User> getAllUser(Integer orggrade,String orgfathername, String orgid,Integer page) {
         if (orggrade == 1) {
-            return userStateDao.getClassAAllUser(orgid,page);//获取一级组织用户
+            return userStateDao.getClassAAllUser(orgfathername,orgid,page);//获取一级组织用户
         }
         if (orggrade == 2) {
-            return userStateDao.getClassBAllUser(orgid,page);//二级组织用户
+            return userStateDao.getClassBAllUser(orgfathername,orgid,page);//二级组织用户
         } else {
-            return userStateDao.getClassCAllUser(orgid,page);
+            return userStateDao.getClassCAllUser(orgfathername,orgid,page);
         }
     }
 
@@ -106,13 +106,13 @@ public class SeeStateService {//按组织查看疫情信息
         return orgList;
     }
 
-    public List<Org> getorgByParentClass(Integer orggrade,String orgid){//获取项目下所有组织
+    public List<Org> getorgByParentClass(Integer orggrade,String orgfathername,String orgid){//获取项目下所有组织
         List<Org> orgList = new ArrayList<>();
         if(orggrade==1){
-            orgList=orgDao.getClassBOrgByParentClass(orgid);
+            orgList=orgDao.getClassBOrgByParentClass(orgfathername,orgid);
         }
         else if(orggrade==2){
-            orgList=orgDao.getClassCOrgByParentClass(orgid);
+            orgList=orgDao.getClassCOrgByParentClass(orgfathername,orgid);
         }
         return orgList;
     }
