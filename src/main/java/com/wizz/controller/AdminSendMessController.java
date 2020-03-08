c5b69de6ea028e823e52b55003b8a767cbd047c8
邹汉章 <45626756+Optimus-Zhang-Prime@users.noreply.github.com>
2020-03-08 10:36:30 +0800

    bug修改3月8日

--- src/main/java/com/wizz/controller/AdminSendMessController.java
+++ src/main/java/com/wizz/controller/AdminSendMessController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.ArrayList;
 import java.util.List;
 
+@Controller
 public class AdminSendMessController {
     @Autowired
     UserDaoImpl userDao;
