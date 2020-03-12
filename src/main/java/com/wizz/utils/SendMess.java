9980f65132579a4b337562df728d5f1a0f33650d
邹汉章 <45626756+Optimus-Zhang-Prime@users.noreply.github.com>
2020-03-12 19:32:27 +0800

    短信修改

--- src/main/java/com/wizz/utils/SendMess.java
+++ src/main/java/com/wizz/utils/SendMess.java
@@ -8,15 +8,15 @@ public class SendMess {
 
     public static void sendCode(String tel,String code) {
         // 短信应用 SDK AppID
-        int appid = 1400325529; // SDK AppID 以1400开头
+        int appid = 1400332073; // SDK AppID 以1400开头
         // 短信应用 SDK AppKey
-        String appkey = "defc2e336cc6cb1162f9dda29829f45d";
+        String appkey = "384586a6b00fb7a5b1711f2a30bf624b";
         // 需要发送短信的手机号码
         String[] phoneNumbers = {tel};
         // 短信模板 ID，需要在短信应用中申请
-        int templateId = 545285; // NOTE: 这里的模板 ID`7839`只是示例，真实的模板 ID 需要在短信控制台中申请
+        int templateId = 553085; // NOTE: 这里的模板 ID`7839`只是示例，真实的模板 ID 需要在短信控制台中申请
         // 签名
-        String smsSign = "吴家骥我的数据可视化分析"; // NOTE: 签名参数使用的是`签名内容`，而不是`签名ID`。这里的签名"腾讯云"只是示例，真实的签名需要在短信控制台申请
+        String smsSign = "疫卡通"; // NOTE: 签名参数使用的是`签名内容`，而不是`签名ID`。这里的签名"腾讯云"只是示例，真实的签名需要在短信控制台申请
         try {
             String[] params = {code};
             SmsSingleSender ssender = new SmsSingleSender(appid, appkey);
@@ -29,15 +29,15 @@ public class SendMess {
     }
     public static void sendNotice(String tel) {//提醒用户打卡
         // 短信应用 SDK AppID
-        int appid = 1400325529; // SDK AppID 以1400开头
+        int appid = 1400332073; // SDK AppID 以1400开头
         // 短信应用 SDK AppKey
-        String appkey = "defc2e336cc6cb1162f9dda29829f45d";
+        String appkey = "384586a6b00fb7a5b1711f2a30bf624b";
         // 需要发送短信的手机号码
         String[] phoneNumbers = {tel};
         // 短信模板 ID，需要在短信应用中申请
-        int templateId = 550275; // NOTE: 这里的模板 ID`7839`只是示例，真实的模板 ID 需要在短信控制台中申请
+        int templateId = 553077; // NOTE: 这里的模板 ID`7839`只是示例，真实的模板 ID 需要在短信控制台中申请
         // 签名
-        String smsSign = "吴家骥我的数据可视化分析"; // NOTE: 签名参数使用的是`签名内容`，而不是`签名ID`。这里的签名"腾讯云"只是示例，真实的签名需要在短信控制台申请
+        String smsSign = "疫卡通"; // NOTE: 签名参数使用的是`签名内容`，而不是`签名ID`。这里的签名"腾讯云"只是示例，真实的签名需要在短信控制台申请
         try {
             String[] params = {};
             SmsSingleSender ssender = new SmsSingleSender(appid, appkey);
@@ -51,15 +51,15 @@ public class SendMess {
     
     public static void sendNoticeToAdmin(String tel,Integer num) {//提醒管理员
         // 短信应用 SDK AppID
-        int appid = 1400325529; // SDK AppID 以1400开头
+        int appid = 1400332073; // SDK AppID 以1400开头
         // 短信应用 SDK AppKey
-        String appkey = "defc2e336cc6cb1162f9dda29829f45d";
+        String appkey = "384586a6b00fb7a5b1711f2a30bf624b";
         // 需要发送短信的手机号码
         String[] phoneNumbers = {tel};
         // 短信模板 ID，需要在短信应用中申请
-        int templateId = 548867; // NOTE: 这里的模板 ID`7839`只是示例，真实的模板 ID 需要在短信控制台中申请
+        int templateId = 553090; // NOTE: 这里的模板 ID`7839`只是示例，真实的模板 ID 需要在短信控制台中申请
         // 签名
-        String smsSign = "吴家骥我的数据可视化分析"; // NOTE: 签名参数使用的是`签名内容`，而不是`签名ID`。这里的签名"腾讯云"只是示例，真实的签名需要在短信控制台申请
+        String smsSign = "疫卡通"; // NOTE: 签名参数使用的是`签名内容`，而不是`签名ID`。这里的签名"腾讯云"只是示例，真实的签名需要在短信控制台申请
         try {
             String[] params = {num.toString()};
             SmsSingleSender ssender = new SmsSingleSender(appid, appkey);
