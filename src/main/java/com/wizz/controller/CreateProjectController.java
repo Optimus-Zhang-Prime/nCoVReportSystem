e3385d603f3c52a1e16a38c4efe5f717941f3d07
邹汉章 <45626756+Optimus-Zhang-Prime@users.noreply.github.com>
2020-03-14 17:06:16 +0800

    删除项目后删除该项目内所有组织

--- src/main/java/com/wizz/controller/CreateProjectController.java
+++ src/main/java/com/wizz/controller/CreateProjectController.java
@@ -1,6 +1,6 @@
 package com.wizz.controller;
 
-
+import com.wizz.entity.Org;
 import com.wizz.service.AdminService;
 import com.wizz.utils.ForumUtils;
 import org.json.JSONException;
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class CreateProjectController {
     @Autowired
     AdminService adminService;
+    @Autowired
+    SeeStateService seeStateService;
 
     @ResponseBody//注册
     @RequestMapping(path = "user/createproject/", method = RequestMethod.POST)
@@ -27,6 +29,11 @@ public class CreateProjectController {
     @RequestMapping(path = "user/deleteproject/", method = RequestMethod.POST)
     public String deleteProject(@RequestParam("projectid") String projectid) throws JSONException {
         Integer code = adminService.deleteProject(projectid);
+        List<Org> orglist=seeStateService.getorgByProject(String projectid);//获取项目下所有组织
+        for(Org org:orglist){//逐个删除
+            String aid=org.get_id;
+            Integer code2 = adminService.deleteOrg(aid);
+        }
         return ForumUtils.toJsonString(code);
     }
 
